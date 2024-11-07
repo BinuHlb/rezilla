0bc2d949e90ae6cdd1bbff6059409aa2c578aa71
BinuHlb <binu.b@hlbhamt.com>
2024-11-07 16:21:09 +0530

    partner page split

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card } from "@/components/ui/card";
 import Hero from './components/sections/hero';
+import Partner from './components/sections/partner';
 
 const properties = [
   {
@@ -99,27 +100,9 @@ function App() {
       
       <Header/>
       <Hero/>
-     
+      <Partner/>
 
-      {/* Partner Logos */}
-      <section className="py-16 bg-gray-50 pt-32">
-  <div className="container mx-auto px-2">
-    <div className="grid grid-cols-2 md:grid-cols-6 gap-4 items-center">
-      {[
-        "https://www.logo.wine/a/logo/Google/Google-Logo.wine.svg",
-        "https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg",
-        "https://www.logo.wine/a/logo/Logitech/Logitech-Logo.wine.svg",
-        "https://www.logo.wine/a/logo/Spotify/Spotify-Logo.wine.svg",
-        "https://www.logo.wine/a/logo/Samsung/Samsung-Logo.wine.svg",
-        "https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
-      ].map((logoUrl, index) => (
-        <div key={index} className="flex justify-center transform transition-transform hover:scale-110 grayscale hover:grayscale-0 opacity-30 hover:opacity-100">
-          <img src={logoUrl} alt={`Partner ${index + 1}`} className="h-30 object-contain" />
-        </div>
-      ))}
-    </div>
-  </div>
-</section>
+    
 {/* Who we are section */}
 <section  className="py-16 bg-gray-50 pt-32">
 <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
---
+++ src/components/sections/partner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Partner: React.FC = () => {
+    return (
+        <section className="py-16 bg-gray-50 pt-32">
+  <div className="container mx-auto px-2">
+    <div className="grid grid-cols-2 md:grid-cols-6 gap-4 items-center">
+      {[
+        "https://www.logo.wine/a/logo/Google/Google-Logo.wine.svg",
+        "https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg",
+        "https://www.logo.wine/a/logo/Logitech/Logitech-Logo.wine.svg",
+        "https://www.logo.wine/a/logo/Spotify/Spotify-Logo.wine.svg",
+        "https://www.logo.wine/a/logo/Samsung/Samsung-Logo.wine.svg",
+        "https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
+      ].map((logoUrl, index) => (
+        <div key={index} className="flex justify-center transform transition-transform hover:scale-110 grayscale hover:grayscale-0 opacity-30 hover:opacity-100">
+          <img src={logoUrl} alt={`Partner ${index + 1}`} className="h-30 object-contain" />
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
+    );
+};
+
+export default Partner;
